a7876ffc0126c5e6c4f615cb47f54d077af87b24
gnudad <gnudad@icloud.com>
2022-11-11 10:22:26 -0500

    Add general.el and some basic keybinds

--- emacs/init.el
+++ emacs/init.el
@@ -15,9 +15,50 @@
 (add-to-list 'default-frame-alist '(undecorated-round . t))
 
 ;;; Evil/Keybinds
+(use-package general)
+
 (use-package evil
-  :init (setq evil-want-keybinding nil)
-  :config (evil-mode))
+  :init
+  (defconst evil-leader "SPC")
+  (setq evil-want-C-u-scroll t
+	evil-want-keybinding nil)
+
+  :config
+  (evil-mode)
+
+  :general
+  (:keymaps 'key-translation-map "ESC" "C-g")
+
+  (:keymaps 'override :states '(normal motion) :prefix evil-leader
+    "u"   'universal-argument
+    "h"   'help-command
+    "w"   'evil-window-map
+
+    "TAB" 'switch-to-buffer
+    "k"   'kill-current-buffer
+
+    "RET" 'execute-extended-command
+    "eb"  'eval-buffer
+    "ed"  'eval-defun
+    "ee"  'eval-expression
+    "er"  'eval-region
+    "es"  'eval-last-sexp
+
+    "nd"  'narrow-to-defun
+    "nr"  'narrow-to-region
+    "ns"  'org-narrow-to-subtree
+    "nw"  'widen)
+
+  (:keymaps 'override
+    "C-h" 'evil-window-left
+    "C-j" 'evil-window-down
+    "C-k" 'evil-window-up
+    "C-l" 'evil-window-right)
+
+  (:keymaps 'override :states 'normal :prefix evil-leader
+    "/"   'comment-line)
+  (:keymaps 'override :states 'motion :prefix evil-leader
+    "/"   'comment-dwim))
 
 (use-package evil-collection
   :config (evil-collection-init))
@@ -25,10 +66,17 @@
 (use-package which-key
   :config (which-key-mode))
 
-;;; Git
+;;; Git/Project
 (use-package magit
   :config (setq magit-display-buffer-function
 		'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package project
+  :general
+  (:keymaps 'override :states '(normal motion) :prefix evil-leader
+    "p" project-prefix-map)
+  (:keymaps 'project-prefix-map
+    "TAB" 'project-switch-to-buffer))
 
 ;;; Misc
 (use-package emacs
