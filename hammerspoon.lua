57c07b90b2caafae692a541d977dc7d5c51fc7df
gnudad <gnudad@icloud.com>
2024-07-02 12:25:00 -0400

    Add window resize binds to hammerspoon config

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -21,6 +21,31 @@ for key, rect in pairs(movements) do
   end)
 end
 
+-- Window Resize
+local function resize(delta)
+  local frame = hs.window.focusedWindow():frame()
+  local screen = hs.screen.mainScreen():fullFrame()
+  if frame.w < screen.w then
+    if frame.x + frame.w == screen.w then
+      frame.x = frame.x - delta
+    elseif frame.x > 0 then
+      frame.x = frame.x - delta/2
+    end
+    frame.w = math.min(frame.w + delta, screen.w)
+  end
+  if frame.h < screen.h - 50 then
+    if frame.y + frame.h > screen.h - 50 then
+      frame.y = frame.y - delta
+    elseif frame.y > 50 then
+      frame.y = frame.y - delta/2
+    end
+    frame.h = math.min(frame.h + delta, screen.h)
+  end
+  hs.window.focusedWindow():setFrame(frame)
+end
+hs.hotkey.bind({ "ctrl", "cmd" }, "=", function() resize(100) end)
+hs.hotkey.bind({ "ctrl", "cmd" }, "-", function() resize(-100) end)
+
 -- Vim Keybinds
 local function vimBind(bindKey, mods, key)
   local stroke = function() hs.eventtap.keyStroke(mods, key, 0) end
