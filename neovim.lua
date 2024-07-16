4fb8fbe2dbe54570e9589b9c9bb6fd8c7516a822
gnudad <gnudad@icloud.com>
2024-07-16 10:06:00 -0400

    Make window switching/resizing work in all vim modes

--- neovim.lua
+++ neovim.lua
@@ -612,14 +612,15 @@ require("lazy").setup({
   { "mrjones2014/smart-splits.nvim", build = "./kitty/install-kittens.bash",
     config = function()
       require("smart-splits").setup({ at_edge = "stop" })
-      vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
-      vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
-      vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
-      vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
-      vim.keymap.set('n', '<C-A-h>', require('smart-splits').resize_left)
-      vim.keymap.set('n', '<C-A-j>', require('smart-splits').resize_down)
-      vim.keymap.set('n', '<C-A-k>', require('smart-splits').resize_up)
-      vim.keymap.set('n', '<C-A-l>', require('smart-splits').resize_right)
+      local modes = { "n", "i", "x", "c" }
+      vim.keymap.set(modes, "<C-h>", require("smart-splits").move_cursor_left)
+      vim.keymap.set(modes, "<C-j>", require("smart-splits").move_cursor_down)
+      vim.keymap.set(modes, "<C-k>", require("smart-splits").move_cursor_up)
+      vim.keymap.set(modes, "<C-l>", require("smart-splits").move_cursor_right)
+      vim.keymap.set(modes, "<C-A-h>", require("smart-splits").resize_left)
+      vim.keymap.set(modes, "<C-A-j>", require("smart-splits").resize_down)
+      vim.keymap.set(modes, "<C-A-k>", require("smart-splits").resize_up)
+      vim.keymap.set(modes, "<C-A-l>", require("smart-splits").resize_right)
     end,
   },
   { "mikesmithgh/kitty-scrollback.nvim", config = true,
