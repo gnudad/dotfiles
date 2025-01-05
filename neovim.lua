cfa441a38452742f233c379e22f537756a59e9b1
gnudad <gnudad@icloud.com>
2025-01-05 09:33:34 -0500

    Auto-format go code on save with conform.nvim

--- neovim.lua
+++ neovim.lua
@@ -366,15 +366,20 @@ require("lazy").setup({
   },
   { "stevearc/conform.nvim",
     dependencies = "williamboman/mason.nvim",
+    ft = { "go" },
     opts = {
-        formatters_by_ft = {
-          css = { "prettier" },
-          go = { "gofmt" },
-          html = { "prettier" },
-          javascript = { "prettier" },
-          json = { "jq" },
-          python = { "ruff_format" },
-        },
+      format_on_save = function(bufnr)
+        if vim.bo[bufnr].filetype ~= "go" then return end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
+      formatters_by_ft = {
+        css = { "prettier" },
+        go = { "gofmt" },
+        html = { "prettier" },
+        javascript = { "prettier" },
+        json = { "jq" },
+        python = { "ruff_format" },
+      },
     },
     keys = {{ "<leader>=", mode = { "n", "x" }, function()
       require("conform").format({ lsp_format = "fallback" })
